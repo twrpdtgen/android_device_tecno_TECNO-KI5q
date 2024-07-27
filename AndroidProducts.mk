26d335f427d377e4acf1e5b9f5db27a214529953
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-07-27 03:40:48 +0200

    TECNO-KI5q: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: tecno Model: TECNO KI5q Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TECNO-KI5q.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TECNO-KI5q-user \
+    omni_TECNO-KI5q-userdebug \
+    omni_TECNO-KI5q-eng
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_TECNO-KI5q-user
+add_lunch_combo omni_TECNO-KI5q-userdebug
+add_lunch_combo omni_TECNO-KI5q-eng
