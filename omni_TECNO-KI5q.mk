212cac0a12836cc910a4b6cf82adeffb21d18082
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-12-12 22:46:41 +0100

    TECNO-KI5q: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: tecno Model: TECNO KI5q Script version: 3.0.0

---
+++ omni_TECNO-KI5q.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from TECNO-KI5q device
+$(call inherit-product, device/tecno/TECNO-KI5q/device.mk)
+
+PRODUCT_DEVICE := TECNO-KI5q
+PRODUCT_NAME := omni_TECNO-KI5q
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := TECNO KI5q
+PRODUCT_MANUFACTURER := tecno
+
+PRODUCT_GMS_CLIENTID_BASE := android-tecno
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vnd_ki5q_h6127-user 12 SP1A.210812.016 678008 release-keys"
+
+BUILD_FINGERPRINT := TECNO/KI5q-GL/TECNO-KI5q:12/SP1A.210812.016/240812V1338:user/release-keys
